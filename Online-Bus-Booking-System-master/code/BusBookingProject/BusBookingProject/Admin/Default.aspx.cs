4b121a81fda91290232f35b45f636f99e5fd15d2
tanvir626 <57144266+tanvir626@users.noreply.github.com>
2023-07-23 13:29:52 +0600

    adam-v20-23/07/2023 pm
    *Added the admin panel with total information such as total bus number ,client number , route number and booked ticket

--- Online-Bus-Booking-System-master/code/BusBookingProject/BusBookingProject/Admin/Default.aspx.cs
+++ Online-Bus-Booking-System-master/code/BusBookingProject/BusBookingProject/Admin/Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,6 +12,46 @@ namespace BusBookingProject.Admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=OnlineBusBooking;Integrated Security=True");
+            //Geting bus number
+            con.Open();
+            SqlCommand com = new SqlCommand("Select count(BusId) from BusMaster",con);
+            SqlDataReader dr = com.ExecuteReader();
+            while(dr.Read())
+            {
+                Label1.Text = dr.GetValue(0).ToString();
+            }
+            con.Close();
+
+            //Getting route number
+            con.Open();
+            SqlCommand com1 = new SqlCommand("Select count(RouteID) from RouteDetails", con);
+            SqlDataReader dr1 = com1.ExecuteReader();
+            while (dr1.Read())
+            {
+                Label3.Text = dr1.GetValue(0).ToString();
+            }
+            con.Close();
+
+            //Getting customer number
+            con.Open();
+            SqlCommand com2 = new SqlCommand("Select count(regId) from Registration", con);
+            SqlDataReader dr2 = com2.ExecuteReader();
+            while (dr2.Read())
+            {
+                Label2.Text = dr2.GetValue(0).ToString();
+            }
+            con.Close();
+
+            //Getting total Sold Ticket
+            con.Open();
+            SqlCommand com3 = new SqlCommand("select sum(BookedSeats) from ScheduleMaster;", con);
+            SqlDataReader dr3 = com3.ExecuteReader();
+            while (dr3.Read())
+            {
+                Label4.Text = dr3.GetValue(0).ToString();
+            }
+            con.Close();
 
         }
     }
